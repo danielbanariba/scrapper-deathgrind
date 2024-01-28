1ca7ea0da9af4d76b94b835f051c49cbffe1535e
Daniel Banariba <danielbanariba@protonmail.com>
2024-01-28 03:52:19 -0600

    Script que genera un html de todo los resultados del html

---
+++ script_busqueda_youtube.py
@@ -0,0 +1,20 @@
+with open('bandas.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+print(f"Leídas {len(lines)} líneas de bandas.txt")
+
+links = []
+for line in lines:
+    line = line.strip()
+    search_query = line.replace(' ', '+')
+    url = f"https://www.youtube.com/results?search_query={search_query}"
+    link = f'<a href="{url}" target="_blank">{line}</a><br>'
+    links.append(link)
+
+print(f"Generados {len(links)} enlaces")
+
+with open('busqueda.html', 'w', encoding='utf-8') as f:
+    for link in links:
+        f.write(link + '\n')
+
+print("Archivo busqueda.html creado")
