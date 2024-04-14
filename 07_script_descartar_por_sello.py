cd453c0db3d65cc3aa43225cc3a295aa65503989
Daniel Banariba <danielbanariba@protonmail.com>
2024-04-14 05:15:57 -0600

    Nueva enumeracion para ordenar mejor los scripts

--- 07_script_descartar_por_sello.py
+++
@@ -1,3 +0,0 @@
-
-#?NUEVA IDEA 
-# Descargar el html de las pagina DeathGrind.club donde este relacionado el sello discografico y simplemente comparar los nombres y descartarlo automaticamente si pertenece a uno de los sellos discograficos mencionados anteriormente
---
+++ 3_descartar_banda_por_sello.py
@@ -0,0 +1,10 @@
+
+#?NUEVA IDEA 
+# Descargar el html de las pagina DeathGrind.club donde este relacionado el sello discografico y simplemente comparar los nombres y descartarlo automaticamente si pertenece a uno de los sellos discograficos mencionados anteriormente
+
+# 
+#TODO descargar el html de las paginas de DeathGrind.club donde este relacionado el sello discografico
+# Century Media Records = https://deathgrind.club/posts/labels/Century%20Media%20Records
+# Nuclear Blast Records = https://deathgrind.club/posts/labels/Nuclear%20Blast%20Records
+# Metal Blade Records = https://deathgrind.club/posts/labels/Metal%20Blade%20Records
+# Unique Leader Records = https://deathgrind.club/posts/labels/Unique%20Leader%20Records
